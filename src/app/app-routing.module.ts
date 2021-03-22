5b9328e0b4710b5e254b268cbf9e39fde523635a
Alejandro Castro Martinez <alejandro.castro2016@outlook.es>
2021-03-21 22:00:00 -0500

    Home page done
    Home page done with routes to Login and Registrarse.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,62 +3,88 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { GruposEstudiantilesComponent } from './components/grupos-estudiantiles/grupos-estudiantiles.component';
 
 const routes: Routes = [
-  { path: 'grupos_estudiantiles', component: GruposEstudiantilesComponent },
+  { path: "grupos_estudiantiles", component: GruposEstudiantilesComponent },
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: "folder/:id",
+    loadChildren: () =>
+      import("./folder/folder.module").then((m) => m.FolderPageModule),
   },
   {
-    path: 'calculadora',
-    loadChildren: () => import('./calculadora/calculadora.module').then( m => m.CalculadoraPageModule)
+    path: "calculadora",
+    loadChildren: () =>
+      import("./calculadora/calculadora.module").then(
+        (m) => m.CalculadoraPageModule
+      ),
   },
   {
-    path: 'reco-grupos',
+    path: "reco-grupos",
     children: [
       {
-        path:"",
-        loadChildren: () => import('./grupos-estudiantiles/reco-grupos/reco-grupos.module').then( m => m.RecoGruposPageModule)
+        path: "",
+        loadChildren: () =>
+          import("./grupos-estudiantiles/reco-grupos/reco-grupos.module").then(
+            (m) => m.RecoGruposPageModule
+          ),
       },
       {
         path: ":grupoID",
-        loadChildren: () => import('./grupos-estudiantiles/reco-grupos/datos-grupo/datos-grupo.module').then( m => m.DatosGrupoPageModule)
-      }
-    ]
+        loadChildren: () =>
+          import(
+            "./grupos-estudiantiles/reco-grupos/datos-grupo/datos-grupo.module"
+          ).then((m) => m.DatosGrupoPageModule),
+      },
+    ],
   },
   {
-    path: 'formulario-perso-grupos',
-    loadChildren: () => import('./grupos-estudiantiles/formulario-perso-grupos/formulario-perso-grupos.module').then( m => m.FormularioPersoGruposPageModule)
+    path: "formulario-perso-grupos",
+    loadChildren: () =>
+      import(
+        "./grupos-estudiantiles/formulario-perso-grupos/formulario-perso-grupos.module"
+      ).then((m) => m.FormularioPersoGruposPageModule),
   },
   {
-    path: 'recomendar-restaurantes',
-    loadChildren: () => import('./recomendar-restaurantes/recomendar-restaurantes.module').then( m => m.RecomendarRestaurantesPageModule)
+    path: "recomendar-restaurantes",
+    loadChildren: () =>
+      import("./recomendar-restaurantes/recomendar-restaurantes.module").then(
+        (m) => m.RecomendarRestaurantesPageModule
+      ),
   },
   {
-    path: 'formulario-perso-restaurantes',
-    loadChildren: () => import('./formulario-perso-restaurantes/formulario-perso-restaurantes.module').then( m => m.FormularioPersoRestaurantesPageModule)
+    path: "formulario-perso-restaurantes",
+    loadChildren: () =>
+      import(
+        "./formulario-perso-restaurantes/formulario-perso-restaurantes.module"
+      ).then((m) => m.FormularioPersoRestaurantesPageModule),
   },
   {
-    path: 'datos-restaurante',
-    loadChildren: () => import('./datos-restaurante/datos-restaurante.module').then( m => m.DatosRestaurantePageModule)
+    path: "datos-restaurante",
+    loadChildren: () =>
+      import("./datos-restaurante/datos-restaurante.module").then(
+        (m) => m.DatosRestaurantePageModule
+      ),
   },
   {
-    path: 'registro',
-    loadChildren: () => import('./auth/registro/registro.module').then( m => m.RegistroPageModule)
+    path: "registro",
+    loadChildren: () =>
+      import("./auth/registro/registro.module").then(
+        (m) => m.RegistroPageModule
+      ),
   },
   {
-    path: 'login',
-    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
-  },  {
-    path: 'home',
-    loadChildren: () => import('./home/home/home.module').then( m => m.HomePageModule)
-  }
-
-
+    path: "login",
+    loadChildren: () =>
+      import("./auth/login/login.module").then((m) => m.LoginPageModule),
+  },
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./home/home/home.module").then((m) => m.HomePageModule),
+  },
 ];
 
 @NgModule({
