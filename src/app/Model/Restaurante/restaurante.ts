dcf2a0993534dc5fb0d8d0813c934b137229953a
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2021-03-27 17:52:55 -0500

    Creación clase TipoRestaurante
    Creación clase TipoRestaurante y se agrego el atributo a la clase Restaurante, al igual que el atributo ambientación

--- src/app/Model/Restaurante/restaurante.ts
+++ src/app/Model/Restaurante/restaurante.ts
@@ -1,12 +1,15 @@
 import { TipoComida } from "../TipoComida/tipo-comida";
+import { TipoRestaurante } from "../TipoRestaurante/tipo-restaurante";
 
 export class Restaurante {
   id: number;
-  tipo: TipoComida[] = [];
+  tipoComida: TipoComida[] = [];
+  tipoRestaurante: TipoRestaurante[] = [];
   calificacion: number;
   regimenAlimenticio: string[] = [];
   franquicia: string;
   tiempoEntrega:number;
+  ambientacion: string;
   constructor(
     public nombre: string,
     public descripcion: string,
