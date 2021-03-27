4a6264da4194c0ed3f7b1f429b9b311c76a8f059
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2021-03-27 18:25:31 -0500

    creación clase Requisito
    se creo la clase Requisito y se agrego el atributo a la clase GrupoEstudiantil

--- src/app/Model/GrupoEstudiantil/grupo-estudiantil.ts
+++ src/app/Model/GrupoEstudiantil/grupo-estudiantil.ts
@@ -1,4 +1,5 @@
 import { Caracteristica } from "../Caracteristica/caracteristica";
+import { Requisito } from "../Requisito/requisito";
 import { Tematica } from "../Tematica/tematica";
 
 export class GrupoEstudiantil {
@@ -6,10 +7,10 @@ export class GrupoEstudiantil {
   tematicas: Tematica[] = [];
   calificacion: number;
   caracteristicas: Caracteristica[] = [];
+  requisitos: Requisito[]=[];
   constructor(
     public descripcion: string,
     public nombre: string,
-    public requisitos: string,
     public perfilGrupo: string
   ) {}
 }
