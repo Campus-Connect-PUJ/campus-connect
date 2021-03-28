042ca9e78de2a4218cefcfd274698c667a015857
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2021-03-28 16:11:49 -0500

    creación servicio RegimenAlimenticio

---
+++ src/app/Model/RegimenAlimenticio/regimen-alimenticio.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { NetService } from 'src/app/utils/net.service';
+import { Observable } from 'rxjs';
+// import { NetService } from '@utils/net.service';
+import { environment } from 'src/environments/environment';
+import { RegimenAlimenticio } from './regimen-alimenticio';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegimenAlimenticioService {
+
+  constructor(private net: NetService) { }
+
+  getRestaurantes(): Observable<RegimenAlimenticio[]> {
+    const url = `${environment.baseUrl}/regimenAlimenticio`;
+    return this.net.get<RegimenAlimenticio[]>(url);
+  }
+
+  getRestauranteById(id: number): Observable<RegimenAlimenticio> {
+    const url = `${environment.baseUrl}/regimenAlimenticio/${id}`;
+    return this.net.get<RegimenAlimenticio>(url);
+  }
+}
