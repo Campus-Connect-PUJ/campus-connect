5c1d1040c33bdbc7165e2cefbbd856f49f950c13
Camilo Muñoz <ancamu99@hotmail.com>
2021-04-05 14:11:28 -0500

    Detalles del monitor

--- src/app/monitores/monitores.page.ts
+++ src/app/monitores/monitores.page.ts
@@ -1,4 +1,6 @@
+import { PopoverController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { MonitorDetallesComponent } from './monitor-detalles/monitor-detalles.component';
 
 @Component({
   selector: 'app-monitores',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonitoresPage implements OnInit {
 
-  constructor() { }
+  constructor(private popoverCtrl:PopoverController) { }
 
   ngOnInit() {
   }
 
+
+  async mostrarInfo(){
+    const popover = await this.popoverCtrl.create({
+      component: MonitorDetallesComponent,
+      cssClass: 'popover',
+      translucent: true
+    }); 
+    return await popover.present();
+  }
 }
